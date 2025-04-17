a44ebf54aa88211c9d84ea948ad53e49fd0bbd0e
HBENG <ben.gatt.hamdi@gmail.com>
2025-04-17 11:31:47 +0200

    Update streamlit_app.py
    Displaying all the information about all choosing fruits

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,7 +27,9 @@ if ingredients_list:
     
     for i in ingredients_list:
         ingredients_st+=i +' '
-
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+        sf_df=st.dataframe(data=smoothiefroot_response.json(),use_container_width=True)
+  
     my_insert_stmt = """ insert into smoothies.public.orders(name_on_order,ingredients)
             values ('""" + name_on_order + """','""" + ingredients_st + """')"""
 
@@ -35,10 +37,3 @@ if ingredients_list:
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
         st.success('Your Smoothie is ordered!', icon="✅")
-
-
-
-
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-#st.text(smoothiefroot_response.json())
-sf_df=st.dataframe(data=smoothiefroot_response.json(),use_container_width=True)
